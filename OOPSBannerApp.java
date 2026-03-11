39ec38fd04355d1a0681b4e0e838a4484a8c6598
poojakeyan0730-ship-it <poojakeyan0730@gmail.com>
2026-03-11 09:15:19 +0530

    UC2: Implemented ASCII Banner OOPS using manual print statements

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp - Use Case 2
+ * Prints the word "OOPS" in large banner format using '*' characters.
+ *
+ * @author Roshan
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****    *****    ***** ");
+        System.out.println("*     *   *     *  *     *  *      ");
+        System.out.println("*     *   *     *  *     *  *      ");
+        System.out.println("*     *   *****    *****    *****  ");
+        System.out.println("*     *   *        *             * ");
+        System.out.println("*     *   *        *             * ");
+        System.out.println(" *****    *        *        *****  ");
+    }
+}
